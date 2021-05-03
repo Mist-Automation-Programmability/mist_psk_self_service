c4defc556904e394b90060e0e960c90f3660bb9e
Thomas Munzer <thomas.munzer@gmail.com>
2021-05-03 10:56:59 +0200

    fix angular user script generation

--- angular-build-user.sh
+++ angular-build-user.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd angular-user/
-ng build --deploy-url static/
-cp -R dist/angular-user ../src/public/
-cp dist/angular-user/index.html ../src/views/user.html
-cd ..
+ng build --deploy-url /ng/user/ --prod
+rm -rf ../src/public/ng/user/*
+cp ./dist/angular-user/* ../src/public/ng/user
+cp ./dist/angular-user/index.html ../src/views/user.html
+
